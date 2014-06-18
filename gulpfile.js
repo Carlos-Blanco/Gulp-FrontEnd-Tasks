be4277749b9a1733c92699b94fd82c3245ba98a9
Carlos Blanco <carlosblanco@me.com>
2014-06-18 18:36:35 +0200

    Add JS concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssMinify = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 /* Compile Sass Task */
 gulp.task('sass', function() {
@@ -15,5 +16,12 @@ gulp.task('cssMinify', function() {
   gulp.src('./*.css')
     .pipe(cssMinify())
     .pipe(gulp.dest('./'));
+});
+
+/* JS Concat */
+gulp.task('concat', function() {
+  gulp.src('./scripts/libs/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./js/'))
 });
 

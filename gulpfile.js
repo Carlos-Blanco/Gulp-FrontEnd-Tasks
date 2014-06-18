2579a926a2b0a28e96fda59e53af25e83dfdca0b
Carlos Blanco <carlosblanco@me.com>
2014-06-18 18:29:16 +0200

    Add CSS Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssMinify = require('gulp-minify-css');
 
+/* Compile Sass Task */
 gulp.task('sass', function() {
   gulp.src('./scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./'));
 });
+
+/* CSS Minify */
+gulp.task('cssMinify', function() {
+  gulp.src('./*.css')
+    .pipe(cssMinify())
+    .pipe(gulp.dest('./'));
+});
+
